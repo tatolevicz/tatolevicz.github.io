bab1c899ecdbbd8705749d569104a9873445efdb
Tato Levicz <tatolevicz@gmail.com>
2021-07-14 18:58:01 -0300

    added first project for real

--- scripts/main.js
+++ scripts/main.js
@@ -4,23 +4,28 @@ var canvas = document.getElementsByTagName('canvas')[0];
 var ctx = canvas.getContext('2d');
 var headerImg = document.getElementsByClassName('header-content-right');
 
-var startX_1 = 100;
+
+var startX_1 = canvas.width/2 - canvas.width/4;
 var startY_1 = 0;
 var freq_1 = 50;
-var amp_1 = 40;
+var amp_1 =  canvas.width/6;
 var phase_1 = 0;
 var speed_1 = 0.08;
 
-var startX_2 = 200;
+var startX_2 = canvas.width/2 + canvas.width/4;
 var startY_2 = 10;
 var freq_2 = 50;
-var amp_2 = 40;
+var amp_2 = canvas.width/6;;
 var phase_2 = 0;
 var speed_2 = 0.08;
 
+
+
+ctx.imageSmoothingEnabled = true;
+
 function animateSins()
 {
-    ctx.clearRect(0,0, 1000,1000);
+    ctx.clearRect(0,0, canvas.width ,canvas.height);
     drawSin(startX_1, startY_1, freq_1, amp_1, (speed_1*phase_1++) % (2*Math.PI));
     drawSin(startX_2, startY_2, freq_2, amp_2, (speed_2*phase_2++) % (2*Math.PI));
     requestAnimationFrame(animateSins);
